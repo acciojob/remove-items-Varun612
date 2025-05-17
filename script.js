060c079558c56480f24f8f7e71c44f1b96108508
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-17 08:38:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+function remove(){
+	var selectList=document.getElementById('colorSelect');
+	var selectedIndex = selectList.selectedIndex;
+	if(selectedIndex !== -1)
+	// selectList = selectList.filter(i => i !== selectedIndex)
+		selectList.remove(selectedIndex);
+}
